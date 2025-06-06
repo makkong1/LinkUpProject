e0160be245ded0e0ed48c290e904f636c2563b57
pyb <wowong123@naver.com>
2025-06-06 18:43:03 +0900

    수정 및 추가

--- src/main/java/kh/link_up/LinkUpApplication.java
+++ src/main/java/kh/link_up/LinkUpApplication.java
@@ -7,13 +7,14 @@ import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.web.filter.HiddenHttpMethodFilter;
 
 @SpringBootApplication
 @MapperScan("kh.link_up.mapper")
 @ServletComponentScan // 이 어노테이션을 추가하여 Servlet 리스너와 필터 자동 등록
-//@EnableScheduling // 주기적 작업을 활성화
+@EnableScheduling // 주기적 작업을 활성화
 @EnableAsync
 @EnableCaching
 //@EnableAdminServer
